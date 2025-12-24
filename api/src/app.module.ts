364a38c55f529971e9d105256d387b3e2dabc35a
gabriel03013 <gm5770283@gmail.com>
2025-12-24 15:52:44 -0300

    chore(api): register api product module

--- api/src/app.module.ts
+++ api/src/app.module.ts
@@ -1,9 +1,12 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { ProductModule } from './product/product.module';
+import { CompleteOrderModule } from './complete-order/complete-order.module';
+
 
 @Module({
-  imports: [],
+  imports: [ProductModule, CompleteOrderModule],
   controllers: [AppController],
   providers: [AppService],
 })
