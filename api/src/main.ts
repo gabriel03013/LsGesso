17d2429ee005d208286b17acbe55f14e571e8b74
gabriel03013 <gm5770283@gmail.com>
2026-01-02 20:42:55 -0300

    fix(api): 502 error

--- api/src/main.ts
+++ api/src/main.ts
@@ -2,10 +2,16 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
+  console.log('🚀 Starting app...');
+
   const app = await NestFactory.create(AppModule);
+
+  console.log('✅ Nest created');
 
   app.enableCors();
 
-  await app.listen(process.env.PORT ?? 3000, '0.0.0.0');
+  const port = process.env.PORT ?? 3000;
+  await app.listen(port, '0.0.0.0');
+
+  console.log(`🔥 Listening on port ${port}`);
 }
-bootstrap();
