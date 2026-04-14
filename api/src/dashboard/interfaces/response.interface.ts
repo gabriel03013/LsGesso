127c502d7fd7c715cd4a349099b43bfaa07980c2
gabriel03013 <gm5770283@gmail.com>
2026-04-14 17:34:19 -0300

    feat(api): add dashboard kpis response interface

---
+++ api/src/dashboard/interfaces/response.interface.ts
@@ -0,0 +1,9 @@
+export interface IResponseDashboardKPIs {
+    title: string,
+    description: string,
+    data: string,
+    type: "money" | "number" | "percentage",
+    icon: string, // TODO: change to available icons enum
+    isTrendingUp?: boolean,
+    trendingPercentage?: number,
+}
