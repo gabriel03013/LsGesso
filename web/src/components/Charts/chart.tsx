bb7a4d89880bd2fc8c546de34045725192be45fb
gabriel03013 <gm5770283@gmail.com>
2026-04-17 17:57:06 -0300

    refactor(web): remove unused chart-types directory, fix BarChartView import

--- web/src/components/Charts/chart.tsx
+++ web/src/components/Charts/chart.tsx
@@ -4,25 +4,25 @@ import AreaChartView from "./area-chart-view";
 import BarChartView from "./bar-chart-view";
 import PieChartView from "./pie-chart-view";
 
-type ChartProps = { chart: ChartResponse<unknown> };
+type ChartProps = { chart: ChartResponse<unknown>; className?: string };
 
-const Chart = ({ chart }: ChartProps) => {
+const Chart = ({ chart, className }: ChartProps) => {
   switch (chart.chartType) {
     case "area":
       return (
-        <ChartCard chart={chart}>
+        <ChartCard chart={chart} className={className}>
           <AreaChartView chart={chart} />
         </ChartCard>
       );
     case "bar":
       return (
-        <ChartCard chart={chart}>
+        <ChartCard chart={chart} className={className}>
           <BarChartView chart={chart} />
         </ChartCard>
       );
     case "pie":
       return (
-        <ChartCard chart={chart}>
+        <ChartCard chart={chart} className={className}>
           <PieChartView chart={chart} />
         </ChartCard>
       );
